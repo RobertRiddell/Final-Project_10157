2ff48e8dadd627e0d019c276e764014f818b232e
RobertRiddell <54061265+RobertRiddell@users.noreply.github.com>
2021-04-26 15:28:51 +1000

    Started modelling have gotten to player normalisation by minutes and have discoverd that team based metrics wont help in evalauting player performace

---
+++ funs/gg_scatter.R
@@ -0,0 +1,8 @@
+gg_scatter <-  function(data, feature) {
+  ggplot(data, aes(x = .data[[feature]], y = pts)) +
+    geom_point(colour = '#048BA8', alpha = 0.5) +
+    geom_smooth(method = 'lm', colour = "#16DB93", se= F) +
+    geom_smooth(colour = "#A4036F", se= F) +
+    ggtitle(deparse(substitute(data)) , feature) +
+    theme_bw()
+}
